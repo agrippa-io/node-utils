bfeaaeee0bd20d46f59cf696a4199f7b0a993f84
Matt Valli <matthew.valli@gmail.com>
2023-11-28 11:55:41 -0800

    Converted to safely typed Typescript

--- src/encrypt/encryptPassword.ts
+++ src/encrypt/encryptPassword.ts
@@ -1,30 +1,19 @@
 import crypto from 'crypto'
 
-export async function encryptPassword(password) {
+export async function encryptPassword(password: string): Promise<Buffer> {
+  return new Promise((resolve, reject) => {
+    crypto.pbkdf2(
+      password,
+      process.env.ENCRYPT_SALT,
+      parseInt(process.env.ENCRYPT_ITERATIONS),
+      parseInt(process.env.ENCRYPT_KEY_LENGTH),
+      process.env.ENCRYPT_DIGEST,
+      (err, hashedInput) => {
+        if (err) {
+          reject(err)
+        }
 
-  console.log('**********************************************')
-  console.log('ENCRYPT_SALT:', process.env.ENCRYPT_SALT ?? '*** EMPTY ***')
-  console.log('ENCRYPT_ITERATIONS:', process.env.ENCRYPT_ITERATIONS ?? '*** EMPTY ***')
-  console.log('ENCRYPT_KEY_LENGTH:', process.env.ENCRYPT_KEY_LENGTH ?? '*** EMPTY ***')
-  console.log('ENCRYPT_DIGEST:', process.env.ENCRYPT_DIGEST ?? '*** EMPTY ***')
-  console.log('**********************************************')
-
-  crypto.pbkdf2(
-    password,
-    process.env.ENCRYPT_SALT,
-    parseInt(process.env.ENCRYPT_ITERATIONS),
-    parseInt(process.env.ENCRYPT_KEY_LENGTH),
-    process.env.ENCRYPT_DIGEST,
-    (err, hashedInput) => {
-      if (err) {
-        console.log('ERROR - Failed to encrypt password')
-        throw err
-      }
-
-      console.log('**********************************************')
-      console.log(`hashedInput[password: '${password}']`, hashedInput.toString())
-      console.log('**********************************************')
-
-      return hashedInput
-    })
+        return resolve(hashedInput)
+      })
+  })
 }
